2bafc1e31ad5da11f46f9347b8c503c6b7f09a2a
Rohit Bhoompally <bhoompally@live.com>
2015-04-17 19:19:20 -0700

    Show movie titles in tableView

--- Tomatoes/MoviesViewController.swift
+++ Tomatoes/MoviesViewController.swift
@@ -8,25 +8,51 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    var movies : [NSDictionary]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let url = NSURL(string: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=dagqdghwaq3e3mxyrp7kmmj5&limit=20&country=US")!
         let request = NSURLRequest(URL: url)
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-            let json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil)
+            let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
+            if let json = json {
+                self.movies = json["movies"] as! [NSDictionary]
+                self.tableView.reloadData()
+            }
             println(json)
         }
-        // Make an async call here.
+
+        tableView.dataSource = self
+        tableView.delegate = self
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = movies {
+            return movies.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let movie = movies![indexPath.row]
+        
+        cell.textLabel?.text = movie["title"] as? String
+        
+        return cell
     }
     
 

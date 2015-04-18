54c46eeb798d08fe2b495b37e9bd3bc57b87efde
Rohit Bhoompally <bhoompally@live.com>
2015-04-17 18:41:28 -0700

    First commit

---
+++ Tomatoes/MoviesViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MoviesViewController.swift
+//  Tomatoes
+//
+//  Created by Rohit Bhoompally on 4/14/15.
+//  Copyright (c) 2015 Rohit Bhoompally. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let urlString = "";
+        
+        // Make an async call here.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
